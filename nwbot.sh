64156cacae446eb5ab32644f317f3109c43c4fde
nessergio <117505925+nessergio@users.noreply.github.com>
2022-12-19 07:28:07 +0400

    Update nwbot.sh

--- nwbot.sh
+++ nwbot.sh
@@ -11,6 +11,6 @@ if [ "$status" != "$old_status" ]; then
         else
             text='%E2%80%BC %D0%A1%D0%B2%D1%96%D1%82%D0%BB%D0%BE%20%D0%B2%D0%B8%D0%BA%D0%BB%D1%8E%D1%87%D0%B8%D0%BB%D0%B8!'
         fi
-    curl -s -X POST https://api.telegram.org/bot$API_ID/sendMessage -d chat_id=$CHAT_ID -d text="$text"
+    curl -s -X POST https://api.telegram.org/bot$API_ID/sendMessage -d chat_id=$CHAT_ID -d text="$text" > /dev/null
     echo $status > $FILE
 fi
